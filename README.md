7ce04dc4c6717f75bb3140ad21e602bd5d451a7b
carlotajuncosa <carlota.juncosa@gmail.com>
2022-11-17 00:25:31 +0100

    FEAT hero.service.ts

--- README.md
+++ README.md
@@ -28,4 +28,5 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
 
 
 ## me quedo aquí 
-https://angular.io/tutorial/toh-pt3 
+https://angular.io/tutorial/toh-pt4
+till Observable data
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Hero } from './../hero';
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
-
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -14,12 +13,20 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
   
-  constructor() { }
+  constructor(private heroService: HeroService) {
+    
+
+  }
   
   ngOnInit(): void {
+    this.getHeroes();
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
